9a931bcda80b754b5c9fce6677cbdc371cc10e12
Theseus <aiqvagent@gmail.com>
2025-03-26 13:17:59 +0100

    Create utils.ts

---
+++ electron/src/frontend/lib/utils.ts
@@ -0,0 +1,49 @@
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+import { atom } from 'jotai'
+
+export function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(inputs))
+}
+
+// Path utils
+function addTrailingSlash(str: string) {
+    if (!str.endsWith('/')) {
+        return str + '/'
+    }
+    return str
+}
+
+function removeTrailingSlash(str: string) {
+    if (str.endsWith('/')) {
+        return str.slice(0, -1)
+    }
+    return str
+}
+
+export function getRelativePath(fullPath: string, projectPath: string) {
+    return fullPath.replace(addTrailingSlash(projectPath), '')
+}
+
+export function getFileName(path: string) {
+    return removeTrailingSlash(path).split('/').pop() || ''
+}
+
+
+export const parseCommand = (content: string) => {
+    const regex = /Running command: (\S+)\s*([\s\S]*)/
+    const match = content.match(regex)
+
+    if (match) {
+        const [, command, remainingText] = match
+
+        return {
+            command: command.trim(),
+            remainingText: remainingText.trim(),
+        }
+    }
+
+    return null // Return null if no command is found
+}
+
+export const savedFolderPathAtom = atom<string | null>(null)
