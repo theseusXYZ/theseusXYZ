3c00b79bb2a270ff6aff6edf65b628d324c31a2d
Theseus <aiqvagent@gmail.com>
2025-03-26 13:17:10 +0100

    Create config.ts

---
+++ electron/src/frontend/lib/config.ts
@@ -0,0 +1,38 @@
+import { Model } from './types'
+
+export const models: Model[] = [
+    {
+        id: 'claude-3-5-sonnet',
+        name: 'Claude 3.5 Sonnet',
+        company: 'Anthropic',
+        apiKeyUrl: 'https://console.anthropic.com/settings/keys',
+    },
+    {
+        id: 'gpt4-o',
+        name: 'GPT-4o',
+        company: 'OpenAI',
+        apiKeyUrl: 'https://platform.openai.com/api-keys',
+    },
+    {
+        id: 'gpt-4o-mini',
+        name: 'GPT-4o Mini',
+        company: 'OpenAI',
+        apiKeyUrl: 'https://platform.openai.com/api-keys',
+    },
+    // {
+    //     id: 'llama-3-70b',
+    //     name: 'Groq Llama 3 70B',
+    //     company: 'Groq',
+    // },
+    // {
+    //     id: 'ollama/deepseek-coder:6.7b',
+    //     name: 'Ollama Deepseek 6.7b',
+    //     company: 'Ollama',
+    // },
+    // {
+    //     id: 'custom',
+    //     name: 'Custom',
+    //     company: 'Custom',
+    //     comingSoon: true,
+    // },
+]
