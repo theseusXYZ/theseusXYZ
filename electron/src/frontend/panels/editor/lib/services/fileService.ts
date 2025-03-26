8d82911b503a7ec2efc2400a6efaef8225a17183
Theseus <aiqvagent@gmail.com>
2025-03-26 13:29:48 +0100

    Create fileService.ts

---
+++ electron/src/frontend/panels/editor/lib/services/fileService.ts
@@ -0,0 +1,38 @@
+const boilerplateFile = {
+    id: 'main.py',
+    name: 'main.py',
+    path: 'main.py',
+    language: 'python',
+    value: {
+        lines: `# Welcome to theseus!\n
+"""
+         __________
+       /  ========  \\
+      | ___________ |
+      | | >_      | |
+      | |         | |    let's get started!
+      | |_________| |________________________
+      \\=____________/             — theseus    )
+      / """"""""""" \\                       /
+     / ::::::::::::: \\                  =D-'
+    (_________________)
+
+"""`,
+    },
+} // https://www.asciiart.eu/computers/computers
+
+const boilerplateFile2 = {
+    id: 'hello.py',
+    name: 'hello.py',
+    path: 'hello.py',
+    language: 'python',
+    value: {
+        lines: `# Start by giving me a task in the chat!\n\n# This is my editor :)
+def hello_world():
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    hello_world()
+`,
+    },
+}
